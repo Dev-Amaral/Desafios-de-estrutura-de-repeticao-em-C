30a1e223add8d38ca4e168d03379b37438b4142f
Davi Amaral <158376236+Dev-Amaral@users.noreply.github.com>
2024-03-27 08:32:33 -0300

    Chico ser maior que Zé.c

--- Chico ser maior que Zé.c
+++ Chico ser maior que Zé.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+/*Chico tem 1,50 metro e cresce 2 centímetros por ano,
+enquanto Zé tem 1,10 metro e cresce 3 centímetros por ano. Construa um algoritmo
+que calcule e imprima quantos anos serão necessários para que Zé seja maior que
+Chico*/
 int main(){
 	int i=0, chico=150, ze=110;
 	while(chico>=ze){
