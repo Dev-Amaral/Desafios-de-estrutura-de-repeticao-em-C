ba71f90cb0ef3495d5d740fe686c268d35ac9640
Davi Amaral <158376236+Dev-Amaral@users.noreply.github.com>
2024-03-27 08:45:14 -0300

    Siglas_de_3_nomes_exercicio_com_String.c

--- Siglas de 3 nomes (exercicio com String).c
+++ Siglas de 3 nomes (exercicio com String).c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+/*Faça um programa que leia três nomes e no final imprima a sigla formada por elas. 
+Por exemplo, se o usuário digitar, Clube, Regatas e Flamengo, deve ser exibido, CRF*/
 int main(){
 	char sig1,sig2,sig3;
 		printf("Digite o primeiro nome\n");
